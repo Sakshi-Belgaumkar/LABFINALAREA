ba68dd20c7aca1f819bb5f49e4112c4cbd04c7e0
Sakshi-Belgaumkar <sakshibelgaumkar04@gmail.com>
2026-01-16 12:15:28 +0530

    save

--- test_area.py
+++ test_area.py
@@ -1,12 +1,2 @@
-import subprocess
-import sys
-
-def test_area_of_circle():
-    result = subprocess.check_output(
-        [sys.executable, "circle.py", "7"],
-        text=True
-    )
-    assert "153.86" in result
-
-if __name__ == "__main__":
-    test_area_of_circle()
+from area import area_of_circle
+assert area_of_circle(12) == 75.36
